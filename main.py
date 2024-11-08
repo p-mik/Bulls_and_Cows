99d0d8c25abecd9abc25fecdc00ce51ebe7c6cc2
p-mik <petr.mikulka@gmail.com>
2024-11-08 21:48:34 +0100

    úvodní zpráva

--- main.py
+++ main.py
@@ -4,3 +4,12 @@ author: Petr Mikulka
 email: petr.mikulka@gmail.com
 discord: p_mik Mik#7555
 """
+
+print("""
+Hi there!
+-----------------------------------------------
+I've generated a random 4 digit number for you.
+Let's play a bulls and cows game.
+-----------------------------------------------
+Enter a number:
+""")
